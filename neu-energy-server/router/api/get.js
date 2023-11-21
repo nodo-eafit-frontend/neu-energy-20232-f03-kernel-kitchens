0617afa489d8a882dbc8e6f209263f948603ddbd
Marvin Cely <mardancely@gmail.com>
2023-11-21 18:50:32 -0500

    feat: Handle error API get

--- neu-energy-server/router/api/get.js
+++ neu-energy-server/router/api/get.js
@@ -3,25 +3,26 @@ const readJson = require('../../utils/json-update');
 require('dotenv').config();
 const { ENERGY_PATH } = process.env;
 
-
 const readEnergy = (req, res) => {
-    const targetYear = Number(req.params.year);
-    const targetMonth = req.params.month;
+  const targetYear = Number(req.params.year);
+  const targetMonth = req.params.month;
 
-    // TODO: Agreguen RECIPE_PATH
-    /*   fs.readFile('./dataEnergy/energyData.json', (err, data) => {
-        if (err) {
-          const msgError = 'Error reading file: ' + err;
-          console.error(msgError);
-          res.status(400).send(msgError);
-        } else {
-          const consumo = JSON.parse(data); */
+  try {
+    // Zona segura
     const consumo = readJson.leerJsonEnergia(ENERGY_PATH);
 
     const consumoMensual = consumo.consumos.find((items) => items.year === targetYear && items.month === targetMonth);
 
-    res.status(200).send(consumoMensual);
-
+    if (consumoMensual) {
+      res.status(200).send(consumoMensual);
+    } else {
+      res.status(404).send({});
+    }
+  } catch (error) {
+    // Atrapamos el error
+    console.error(error);
+    res.status(500).send({});
+  }
 };
 
 module.exports = { readEnergy };
