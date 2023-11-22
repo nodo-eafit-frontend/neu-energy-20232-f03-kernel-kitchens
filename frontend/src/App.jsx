4c217252f72b3e9949fb4ee7366c1badcfab5722
Marvin Cely <mardancely@gmail.com>
2023-11-22 18:41:11 -0500

    feat: review sass

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import './App.scss'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <h1>Hello World</h1>
-  )
+  return <h1>Hello World</h1>;
 }
 
-export default App
+export default App;
