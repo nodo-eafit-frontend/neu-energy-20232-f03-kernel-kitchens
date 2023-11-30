d10a1afe53f6e109f27dd017ab2dc71c32670ce8
Marvin Cely <mardancely@gmail.com>
2023-11-29 19:30:03 -0500

    feat: code review server

--- neu-energy-server/router/api/get.js
+++ neu-energy-server/router/api/get.js
@@ -1,6 +1,4 @@
-const fs = require('fs');
 const readJson = require('../../utils/json-update');
-const { get } = require('http');
 require('dotenv').config();
 const { ENERGY_PATH } = process.env;
 
@@ -11,60 +9,16 @@ const readEnergy = (req, res) => {
   try {
     // Zona segura
     const consumo = readJson.leerJsonEnergia(ENERGY_PATH);
-
     const consumoMensual = consumo.consumos.find((items) => items.year === targetYear && items.month === targetMonth);
 
-    // TODO:
     // 1: Crear método que retorne el valor de enregía más grande getMaxEnergy(consumoMensual): number
-
-    const getMaxEnergy = (consumoMensual) => {
-      let maxEnergy = 0;
-      // empieza en 0 la busqueda del dato mayor
-      consumoMensual.forEach((item) => {
-      // forEach es el iterador para cada elemento del array
-        if (item.energy > maxEnergy) {
-          maxEnergy = item.energy;
-        }
-      });
-      return maxEnergy;
-    };
-
-
+    const maxEnergy = getMaxEnergy(consumoMensual);
 
     // 2: Modificar la respuesta de las horas, agregando el atributo de porcentaje injectEnergyPercent(consumoMensual, maxEnergy): consumoMensualModified
-     const injectEnergyPercent2 = (consumoMensual, maxEnergy) => {
-       consumoMensual.forEach((item) => {
-        item.percent = (item.energy / maxEnergy) * 100;
-      });
-      return consumoMensual;
-     };
-     //dudas de si esta bien
+    const days = injectEnergyPercent(consumoMensual, maxEnergy);
 
-    
-/*     days: [
-      {
-        day: 1,
-        consumption_hour: [
-          { energy: 192, percent: 0.7 },
-          { energy: 92, percent: 0.45 },
-          { energy: 137, percent: 0.7 },
-        ]
-      },
-      {...},
-      {...}
-    ]  */
-
-
-    const injectEnergyPercent = (consumoMensual, maxEnergy) => {
-      const percent = (consumoMensual/maxEnergy) * 100
-      const consumption_hour = hours.map((item) => {
-        return {energy: item, percent: percent}
-      })
-      return consumption_hour
-    } //devuelve energia y porcentaje de las 24h de un dia 
-
-    if (consumoMensual) {
-      res.status(200).send(consumoMensual);
+    if (days?.length) {
+      res.status(200).send({ year: targetYear, month: targetMonth, days });
     } else {
       res.status(404).send({});
     }
---
+++ neu-energy-server/utils/energy.js
@@ -0,0 +1,32 @@
+const getMaxEnergy = (consumoMensual) => {
+  let maxEnergy = 0;
+  // empieza en 0 la busqueda del dato mayor
+  consumoMensual.days.forEach((day) => {
+    // forEach es el iterador para cada elemento del array
+    day.hours.forEach((energy) => {
+      if (energy > maxEnergy) {
+        maxEnergy = energy;
+      }
+    });
+  });
+
+  return maxEnergy;
+};
+
+const getConsuptionHour = (hours, maxEnergy) => {
+  const consumption_hour = hours.map((energy) => {
+    return { energy, percent: energy / maxEnergy };
+  });
+
+  return consumption_hour;
+};
+
+const injectEnergyPercent = (consumoMensual, maxEnergy) => {
+  const daysModified = consumoMensual.days.map((item) => {
+    const consumption_hour = getConsuptionHour(item.hours, maxEnergy);
+
+    return { day: item.day, consumption_hour };
+  });
+
+  return daysModified;
+};
